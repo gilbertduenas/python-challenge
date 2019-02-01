bb51f238a3749dc702f3d175c8947b6a5ca5f80f
Gilbert <gilbertduenas@users.noreply.github.com>
2019-02-01 08:14:21 -0800

    Add files via upload

---
+++ PyBank/main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# import modules
+import os
+import csv
+
+# load csv into output_path
+file = "budget_data.csv"
+output = os.path.join(".", file)
+
+# declare lists
+date = []
+profit_loss = []
+
+#read the csv
+with open(output, newline = "", encoding="utf8") as file:
+    data = csv.reader(file, delimiter = ',')
+    for row in data:
+        date.append(row[0])
+        profit_loss.append(row[1])
+
+# zip lists
+budget = list(zip(date, profit_loss))
+
+# remove the headers
+del date[0]
+del profit_loss[0]
+
+# convert to int
+profit_loss = list(map(int, profit_loss))
+
+#header
+print(
+    '''
+Financial Analysis
+----------------------------''')
+
+#The total number of months included in the dataset
+#use len
+date_len = len(date)
+print("Total months: " + str(date_len))
+
+#The net total amount of "Profit/Losses" over the entire period
+#use sum
+profit_loss_sum = sum(profit_loss)
+print("Total: $" + str(profit_loss_sum))
+
+#The average of the changes in "Profit/Losses" over the entire period
+# declare a list to store change values
+average_change = []
+
+#loop through list starting at second index
+for i in range(1, len(profit_loss)):
+    #subtract the previous index from the current index and store the change to a variable
+    change = profit_loss[i] - profit_loss[i-1]
+    #append the change to the average change list
+    average_change.append(change)
+    
+#calculate the average and format to two decimal places
+profit_loss_average = round(sum(average_change) / len(average_change), 2)
+print("Average Change:" + str(profit_loss_average))
+
+#The greatest increase in profits (date and amount) over the entire period
+date_increase_index = (average_change.index(max(average_change)))
+print("Greatest Increase in Profits: " + date[date_increase_index + 1] + " ($" + str(max(average_change)) + ')')
+
+#The greatest decrease in losses (date and amount) over the entire period
+date_decrease_index = (average_change.index(min(average_change)))
+print("Greatest Decrease in Profits: " + date[date_decrease_index + 1] + " ($" + str(min(average_change)) + ')')
